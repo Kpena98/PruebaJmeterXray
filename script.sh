77b200ac9e78be8edd9a8c5d52d6cddc017c51d4
Kpena98 <82413094+Kpena98@users.noreply.github.com>
2022-04-13 17:17:00 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,6 +5,8 @@ reportFile=$3
 varDate=$(date +%c)
 rm $reportFile
 docker run --rm -v $WORKSPACE:/workspace swethapn14/repo_perf:JmeterLatest -Jjmeterengine.stopfail.system.exit=true -Jcsvfile=/workspace/$csvFile -n -t /workspace/$projectName -l /workspace/$reportFile
+rm reporteconvertido.xml
+java -jar $WORKSPACE/jmeter-junit-xml-converter-0.0.1-SNAPSHOT-jar-with-dependencies.jar reportejenkins.jtl reporteconvertido.xml
 if grep "false" $reportFile > resultadoemail.txt && echo "El Nombre del Job es:" $JOB_NAME >> resultadoemail.txt && echo "La fecha y hora de la ejecucion fue:" $varDate >> resultadoemail.txt && echo "El test de perfomance fallo" >> resultadoemail.txt
 then 
 echo El test fallo
