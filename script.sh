45a8792c21cb0648d5a2859333003376fb32e2ed
Kpena98 <82413094+Kpena98@users.noreply.github.com>
2022-04-20 12:30:20 -0300

    Se agrego renombrar reporte de pdf

--- script.sh
+++ script.sh
@@ -7,6 +7,7 @@ rm $reportFile
 docker run --rm -v $WORKSPACE:/workspace swethapn14/repo_perf:JmeterLatest -Jjmeterengine.stopfail.system.exit=true -Jcsvfile=/workspace/$csvFile -n -t /workspace/$projectName -l /workspace/$reportFile
 rm reporteconvertido.xml
 java -jar $WORKSPACE/jmeter-junit-xml-converter-0.0.1-SNAPSHOT-jar-with-dependencies.jar reportejenkins.jtl reporteconvertido.xml
+mv $WORKSPACE/alternate_reporteconvertido.xml $WORKSPACE/reportefinal.xml
 if grep "false" $reportFile > resultadoemail.txt && echo "El Nombre del Job es:" $JOB_NAME >> resultadoemail.txt && echo "La fecha y hora de la ejecucion fue:" $varDate >> resultadoemail.txt && echo "El test de perfomance fallo" >> resultadoemail.txt
 then 
 echo El test fallo
