dd59fd43097826807391095c4dda04a92579ac33
Kpena98 <82413094+Kpena98@users.noreply.github.com>
2022-04-28 13:12:04 -0300

    Update ScriptHtml.sh

--- ScriptHtml.sh
+++ ScriptHtml.sh
@@ -8,10 +8,10 @@ JMETERPLUGINSCMD=JMeterPluginsCMD.sh
 jmeter -n -t  $WORKSPACE/test.jmx -l reportejenkins.jtl -e -o dashboard
  
 # process JTL and covert it to a synthesis report as CSV
-$JMETERPLUGINSCMD --generate-csv synthesis_results.csv --input-jtl results.jtl --plugin-type SynthesisReport
-$JMETERPLUGINSCMD --tool Reporter --generate-csv reports/aggregate_results.csv --input-jtl results.jtl --plugin-type AggregateReport
+$JMETERPLUGINSCMD --generate-csv synthesis_results.csv --input-jtl reportejenkins.jtl --plugin-type SynthesisReport
+$JMETERPLUGINSCMD --tool Reporter --generate-csv reports/aggregate_results.csv --input-jtl reportejenkins.jtl --plugin-type AggregateReport
  
-$JMETERPLUGINSCMD --generate-png reports/ResponseTimesOverTime.png --input-jtl results.jtl --plugin-type ResponseTimesOverTime --width 800 --height 600
-$JMETERPLUGINSCMD --generate-png reports/TransactionsPerSecond.png --input-jtl results.jtl --plugin-type TransactionsPerSecond --width 800 --height 600
+$JMETERPLUGINSCMD --generate-png reports/ResponseTimesOverTime.png --input-jtl reportejenkins.jtl --plugin-type ResponseTimesOverTime --width 800 --height 600
+$JMETERPLUGINSCMD --generate-png reports/TransactionsPerSecond.png --input-jtl reportejenkins.jtl --plugin-type TransactionsPerSecond --width 800 --height 600
 
 java -jar $WORKSPACE/jmeter-junit-xml-converter-0.0.1-SNAPSHOT-jar-with-dependencies.jar reportejenkins.jtl reporteconvertido.xml
